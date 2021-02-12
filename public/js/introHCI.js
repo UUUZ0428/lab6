8a7a9142ac8a74e195a2c4770dc1c9f70b719a17
Yuzi Chu <ychu@ucsd.edu>
2021-02-11 17:25:58 -0800

    added FB and description

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,6 +11,15 @@ $(document).ready(function() {
 function initializePage() {
 	$('.project a').click(addProjectDetails);
 
+	/*
+	$(".project a").on("click", funtion(){
+		e.preventDefault();
+		console.log(idNumber);
+	});
+
+	*/
+	$(".project details").click(projectClick)
+
 	// $('#colorBtn').click(randomizeColors);
 }
 
@@ -27,4 +36,28 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var idNumberURL = "http://localhost:3000/project/" + idNumber;
+	console.log(idNumberURL);
+	$.get(idNumberURL, callBackFn);
+	//$.post(idNumberURL,  {"json":"json"}, callBackFn)
+}
+
+function projectClick(e){
+	e.preventDefault();
+	//$.get("/project/random",addProject);
+
+}
+
+function callBackFn(result){
+	console.log(result);
+
+	var projectDetail = '<img src = "' + result['image'] + '" + class="detailsImage">' + '<p>' + result['title']
+	+ '</p>' + '<p><smail>' + result['date'] + '</small></p>' + '<p>' + result['summary'] + '</p>';
+
+	//$("#1 .details").html(htmlDetail);
+	//$("#boy").click(console.log("clicked"));
+	$(".details").html(projectDetail);
+	//$("#project-description").html(result['summary']);
+
 }
